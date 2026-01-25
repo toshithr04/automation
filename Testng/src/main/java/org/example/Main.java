e3625500ee92128cda7851a05e4ad8c6b5e99e5d
R <vrtosh@amazon.com>
2026-01-25 16:56:40 +0530

    start Automation

---
+++ Testng/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+    }
+}
