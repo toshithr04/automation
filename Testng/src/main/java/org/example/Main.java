583afc037b97c7e22585ad6a0a6a0240073826e9
R <vrtosh@amazon.com>
2026-02-28 14:05:36 +0530

    Add

--- Testng/src/main/java/org/example/Main.java
+++ Testng/src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+
+import com.google.common.annotations.VisibleForTesting;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.in/");
-        driver.manage().window().maximize();
+public class Main extends BaseClass{
+
+    @Test
+    public void Login(){
+        System.out.println("hi");
+        driver.get("https://www.salesforce.com/form/signup/elf-v2-login/");
     }
+
+    @Test
+    public void Logout(){
+        System.out.println("logout");
+        driver.get("https://www.salesforce.com/form/signup/elf-v2-login/");
+
+    }
+
+
+
+
+
 }
