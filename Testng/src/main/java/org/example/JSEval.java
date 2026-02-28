192e351e533cbb65d69bce4252cc2eb8415326ac
R <vrtosh@amazon.com>
2026-02-28 15:35:06 +0530

    moved files

--- Testng/src/main/java/org/example/JSEval.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class JSEval {
-
-    public static String EvalXpath(String xpath){
-        return "return document.evaluate(\"" + xpath + "\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;";
-    }
-}
